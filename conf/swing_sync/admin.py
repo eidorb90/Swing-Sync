7a9624f4645af3121d6150b3a83f97ae532fc0a5
Brodie <brodieman500@gmail.com>
2025-03-24 20:50:23 -0500

    restarting project

--- conf/swing_sync/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import User
-
-@admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'created_at', 'id')
-    search_fields = ('username', 'created_at', 'id') 
-    list_filter = ('created_at', 'id')
--- conf/swing_sync/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SwingSyncConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'swing_sync'
