2d9fc2545a630f9320746fc48ea7c8514ff80c75
slipgamer8241 <marcussweet0211@gmail.com>
2025-04-08 13:28:30 -0500

    Update React and React Router dependencies; refactor routing in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router";
+import SignInContainer from './layouts/signin.jsx'
+import SignUpContainer from './layouts/signup.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/account" element={<App />} />
+        <Route path="/account/signin" element={<SignInContainer/>} />
+        <Route path="/account/signup" element={<SignUpContainer/>} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
