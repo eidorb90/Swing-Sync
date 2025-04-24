5293172d71648d639a4c35fbacd09012d12743f9
Brodie <brodieman500@gmail.com>
2025-04-24 13:33:41 -0500

    feat: refactor User model to extend AbstractUser and update AUTH_USER_MODEL in settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -21,6 +21,9 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+AUTH_USER_MODEL = "api.User"
+
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
